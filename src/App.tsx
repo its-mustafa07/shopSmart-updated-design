b4c19474b0f6a9a97a1e995f8a02c39165834831
Mubashir Hussin <mubashirqureshi00786@gmail.com>
2023-10-30 14:37:52 +0500

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,88 +1,28 @@
 import React, {useState} from 'react';
-import {
-  ThemeContext,
-  darkBlue,
-  darkPink,
-  lightBlue,
-  lightPink,
-} from './components/themes/theme';
+import {ThemeContext, lightPink} from './components/themes/theme';
 import ThemeSwitcher from './components/themes/themeSwitcher';
-import GradientComponent from './components/Gradient';
-import ErrorPage from './screens/error/index';
-import CustomButton from './utils/button';
-import FeedBack from './screens/feedback';
-import Settings from './screens/setting';
-import {Text} from 'react-native';
-import PrivacyPolicy from './screens/privacy-policy';
-import HomeScreen from './screens/home';
-import ProfileHeader from './components/profile-header';
 import HomeItem from './components/home-item';
 import LoginSignup from './screens/login-signup';
-import GridItem from './utils/grid-item';
-import GridViewItems from './components/grid-view-items';
 import ScanningPage from './screens/scanning-page';
-import ListItem from './components/list-item';
-import CartScreen from './screens/cart';
-import HomeStockScreen from './screens/home-stock';
-import HomeStockListScreen from './screens/home-stock-list-screen';
-import ToFillStockListScreen from './screens/toFill-stock-list-screen';
-import OptionsModal from './components/options-modal';
-
+import ErrorPage from './screens/error';
 import DoneComponent from './screens/done';
-import FAQList from './components/faq-component';
 import FAQ from './screens/faq';
-import BasicInfo from './screens/basic-info';
-import BackgroundGradient from './components/background-gradient';
+import GridViewItems from './components/grid-view-items';
+import ListItem from './components/list-item';
 
 const App = () => {
-  const [currentTheme, setCurrentTheme] = useState(darkBlue);
+  const [currentTheme, setCurrentTheme] = useState(lightPink);
 
   return (
     <ThemeContext.Provider
       value={{theme: currentTheme, setTheme: setCurrentTheme}}>
-      {/* <ThemeSwitcher /> */}
-      {/* <GradientComponent /> */}
-<<<<<<< HEAD
-<
-      <ProfileHeader />
-      <HomeItem />
-=======
+      <ThemeSwitcher />
       {/* <LoginSignup /> */}
-      {/* <DoneComponent /> */}
-      {/* <CustomButton /> */}
-      {/* <FeedBack /> */}
-      {/* <Settings /> */}
-      {/* <PrivacyPolicy /> */}
-      {/* <ErrorPage /> */}
-      {/* <HomeScreen /> */}
-      {/* <ListTitleBar /> */}
-      {/* <ThemeSwitcher /> */}
-      {/* <ListItem /> */}
-      {/* <HomeScreen /> */}
-      {/* <ListTitleBar /> */}
-      {/* <ThemeSwitcher /> */}
-
-      {/* <GradientComponent /> */}
-      {/* <ProfileHeader /> */}
-      {/* <HomeItem /> */}
-      <LoginSignup />
-      {/* <GridViewItems /> */}
-      {/* <FAQList /> */}
-      {/* <FAQ /> */}
       {/* <ScanningPage /> */}
       {/* <ErrorPage /> */}
-      {/* </BackgroundGradient> */}
-      {/* <DoneComponent /> */}
-      {/* <ListItem item={'Atta'} /> */}
-      {/* <DoneComponent /> */}
-      {/* <ListItem /> */}
-      {/* <CartScreen /> */}
-      {/* <HomeStockScreen /> */}
-      {/* <HomeStockListScreen /> */}
-      {/* <OptionsModal /> */}
-      {/* <ConfirmationModal /> */}
-      {/* <ToFillStockListScreen /> */}
->>>>>>> 521633d9162a3166b9942ad4308234af0e6830df
+      <DoneComponent />
+      {/* <FAQ /> */}
+      {/* <GridViewItems /> */}
     </ThemeContext.Provider>
   );
 };
